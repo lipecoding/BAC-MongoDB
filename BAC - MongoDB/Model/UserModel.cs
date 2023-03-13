34195d44f944f996d32f060d0fa95fbe185a9ecd
Felipe Reis <felipe-fleming@hotmail.com>
2023-03-13 14:06:21 -0300

    Update UserDTO to UserModel

---
+++ BAC - MongoDB/Model/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAC___MongoDB.Model
+{
+    internal class UserDTO
+    {
+        public string Account { get; set; }
+        public string Name { get; set; }
+        public string Balance { get; set; }
+        public string Currency { get; set; }
+        public string Agency { get; set; }
+    }
+}
